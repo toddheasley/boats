e2051cf4d800b6e3b5d68a585f0e1743bc105fc3
Todd Heasley <toddheasley@me.com>
2015-09-02 17:11:27 -0400

    Add Xcode project

---
+++ Boats/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Boats
+//
+//  (c) 2015 @toddheasley
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ BoatsTests/BoatsTests.swift
@@ -0,0 +1,29 @@
+//
+//  BoatsTests.swift
+//  BoatsTests
+//
+//  (c) 2015 @toddheasley
+//
+
+import XCTest
+@testable import Boats
+
+class BoatsTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        
+    }
+    
+    func testPerformanceExample() {
+        self.measureBlock {
+            
+        }
+    }
+}
