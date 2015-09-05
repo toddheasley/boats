c89e62dd097fee585554bb865c0d4e5563fc414e
Todd Heasley <toddheasley@me.com>
2015-09-05 19:39:49 -0400

    Add non-local data test

---
+++ BoatsTests/DataTests/DataTests.swift
@@ -0,0 +1,60 @@
+//
+//  DataTests.swift
+//  BoatsTests
+//
+//  (c) 2015 @toddheasley
+//
+
+import XCTest
+@testable import Boats
+
+class DataTests: XCTestCase {
+    func testRefresh() {
+        let expectation = expectationWithDescription("")
+        let data = Data(local: false)
+        data.refresh(){ error in
+            guard let _ = data.JSON as? [String: [AnyObject]] else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            XCTAssertTrue(data.routes.count > 0)
+            expectation.fulfill()
+        }
+        let localExpectation = expectationWithDescription("Local")
+        let localData = Data(local: true)
+        localData.refresh(){ error in
+            guard let _ = localData.JSON as? [String: [AnyObject]] else {
+                XCTFail()
+                localExpectation.fulfill()
+                return
+            }
+            XCTAssertTrue(localData.routes.count > 0)
+            localExpectation.fulfill()
+        }
+        waitForExpectationsWithTimeout(10.0){ error in
+            if let error = error {
+                XCTFail("\(error)")
+            }
+        }
+    }
+    
+    func testJSONEncoding() {
+        let expectation = expectationWithDescription("")
+        let data = Data(local: true)
+        data.refresh(){ error in
+            guard let _ = data.JSON as? [String: [AnyObject]] else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            XCTAssertTrue((data.JSON as! [String: [AnyObject]])["routes"]?.count > 0)
+            expectation.fulfill()
+        }
+        waitForExpectationsWithTimeout(10.0){ error in
+            if let error = error {
+                XCTFail("\(error)")
+            }
+        }
+    }
+}
