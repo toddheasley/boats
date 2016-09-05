7c4fbe48a2b62d6f20f89a579d974edd205917ac
Todd Heasley <toddheasley@me.com>
2016-09-05 16:40:37 -0400

    Add complication templates

--- BoatsTimeExtension/ComplicationController.swift
+++ BoatsTimeExtension/ComplicationController.swift
@@ -15,7 +15,32 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     }
     
     func getPlaceholderTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-        handler(nil)
+        switch complication.family {
+        case .modularSmall:
+            let template = CLKComplicationTemplateModularSmallStackImage()
+            template.line1ImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "ModularStack")!)
+            template.line2TextProvider = CLKSimpleTextProvider(text: "--:--")
+            handler(template)
+        case .modularLarge:
+            let template = CLKComplicationTemplateModularLargeStandardBody()
+            template.headerImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "ModularLarge")!)
+            template.headerTextProvider = CLKSimpleTextProvider(text: "--:--")
+            template.body1TextProvider = CLKSimpleTextProvider(text: "Destination")
+            template.body2TextProvider = CLKSimpleTextProvider(text: "")
+            handler(template)
+        case .utilitarianSmallFlat:
+            let template = CLKComplicationTemplateUtilitarianSmallFlat()
+            template.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "UtilitarianFlat")!)
+            template.textProvider = CLKSimpleTextProvider(text: "--:--")
+            handler(template)
+        case .utilitarianLarge:
+            let template = CLKComplicationTemplateUtilitarianLargeFlat()
+            template.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "UtilitarianFlat")!)
+            template.textProvider = CLKSimpleTextProvider(text: "--:-- Destination")
+            handler(template)
+        default:
+            handler(nil)
+        }
     }
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
