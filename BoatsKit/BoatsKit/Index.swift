f4a1b761ee46f5604106d54dcc806731a582fe63
toddheasley <toddheasley@me.com>
2018-11-18 22:59:31 -0500

    Add index model

--- BoatsKit/BoatsKit/Index.swift
+++ BoatsKit/BoatsKit/Index.swift
@@ -1,19 +1,24 @@
 import Foundation
 import CoreLocation
 
-public struct Index: CustomStringConvertible {
-    
+public struct Index: CustomStringConvertible, Codable {
     public private(set) var name: String
     public private(set) var description: String
-    public private(set) var location: Location
     public private(set) var url: URL
+    public private(set) var location: Location
     public private(set) var routes: [Route]
     
     public init() {
         name = "Casco Bay Lines"
         description = "Ferry Schedules"
+        url = URL(string: "https://www.cascobaylines.com")!
         location = .portland
-        url = URL(string: "https://www.cascobaylines.com/")!
-        routes = []
+        routes = Route.allCases
+    }
+    
+    func scheduleURL(for route: Route, season: Season.Name) -> URL? {
+        return URL(string: "\(url)/schedules/\(route.uri)-schedule/\(season.rawValue)")
     }
 }
+
+
--- BoatsKit/BoatsKitTests/IndexTests.swift
+++ BoatsKit/BoatsKitTests/IndexTests.swift
@@ -2,5 +2,18 @@ import XCTest
 @testable import BoatsKit
 
 class IndexTests: XCTestCase {
+    func testInit() {
+        XCTAssertEqual(Index().name, "Casco Bay Lines")
+        XCTAssertEqual(Index().description, "Ferry Schedules")
+        XCTAssertEqual(Index().url, URL(string: "https://www.cascobaylines.com"))
+        XCTAssertEqual(Index().location, .portland)
+        XCTAssertEqual(Index().routes, Route.allCases)
+    }
     
+    func testRouteScheduleURL() {
+        XCTAssertEqual(Index().scheduleURL(for: .peaks, season: .spring), URL(string: "https://www.cascobaylines.com/schedules/peaks-island-schedule/spring"))
+        XCTAssertEqual(Index().scheduleURL(for: .littleDiamond, season: .summer), URL(string: "https://www.cascobaylines.com/schedules/little-diamond-island-schedule/summer"))
+        XCTAssertEqual(Index().scheduleURL(for: .greatDiamond, season: .fall), URL(string: "https://www.cascobaylines.com/schedules/great-diamond-schedule/fall"))
+        XCTAssertEqual(Index().scheduleURL(for: .diamondCove, season: .winter), URL(string: "https://www.cascobaylines.com/schedules/diamond-cove-schedule/winter"))
+    }
 }
